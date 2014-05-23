58afae28fc659f11b5582db189de72d7452156d3
flucas <faculucas@gmail.com>
2014-05-23 01:19:55 -0300

    ahora la app ya tiene un login basico, falta agregar usuarios en base de datos

--- src/main/java/ar/com/softarte/controllers/Home.java
+++ src/main/java/ar/com/softarte/controllers/Home.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,23 +20,35 @@ public class Home {
 	@Autowired
 	private HibernateFunctionsBO hibernateFunctionsDAO;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
     public ModelAndView displayHome() {
         return new ModelAndView("inicio");
     }
-	
-	@RequestMapping("/home")
-	public ModelAndView showMessage() {
-		
-		return new ModelAndView("inicio");
-		
-	}
-	
 	
 	@RequestMapping(value = "/getGraficoVentasXMes", method = RequestMethod.GET)
 	public @ResponseBody List<GVentasXMes> getGVentasXMes() {
 		//prueba de comentario git
 		return hibernateFunctionsDAO.list();
 		
+	}
+	
+	//Spring Security see this :
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+		@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "logout", required = false) String logout) {
+ 
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+ 
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+ 
+		return model;
+ 
 	}
 }
--- src/main/webapp/pages/tiles-defs/menu.jsp
+++ src/main/webapp/pages/tiles-defs/menu.jsp
@@ -1,8 +1,10 @@
+<%@ taglib uri="/WEB-INF/tlds/c.tld" 				prefix="c"%>
+
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Caja Pastelera</a>
+      <a class="navbar-brand" href="#">eCuentas</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -22,7 +24,23 @@
             <li><a href="#">Tipos de Medidas</a></li>
           </ul>
         </li>
+        <li id="logout"><a href="javascript:formSubmit()" />Logout</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+
+<c:url value="/j_spring_security_logout" var="logoutUrl" />
+
+<!-- csrt for log out-->
+<form action="${logoutUrl}" method="post" id="logoutForm">
+  <input type="hidden" 
+	name="${_csrf.parameterName}"
+	value="${_csrf.token}" />
+</form>
+ 
+<script>
+	function formSubmit() {
+		document.getElementById("logoutForm").submit();
+	}
+</script>
